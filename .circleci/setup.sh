c75897e38984f167ff6a2bd1d1efe531f6238162
Ranjeet Singh <ranjeet@raeoks.com>
2018-01-23 17:24:28 +0530

    re-factor deploy step to for automated deploys

--- .circleci/setup.sh
+++ .circleci/setup.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+git config --global user.email "${GITHUB_EMAIL}" > /dev/null 2>&1
+git config --global user.name "${GITHUB_NAME}" > /dev/null 2>&1
+
 cat >> ~/.ssh/config << EOF
 VerifyHostKeyDNS yes
 StrictHostKeyChecking no
